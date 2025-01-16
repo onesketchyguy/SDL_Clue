796152a14cd65122d38791008649b103607956c0
Forrest H Lowe <lowebros9812@gmail.com>
2025-01-16 10:42:45 -0800

    Fixed editor not aligning with mapview

--- src/suspect.cpp
+++ src/suspect.cpp
@@ -19,13 +19,6 @@ void Suspect::DrawMini(gobl::vec2<float> pos, int scale)
 	int col = sprIndex % spriteData.cols;
 	int row = sprIndex / spriteData.cols;
 
-	SDLWrapper::DrawSprite(spriteData.name, gobl::vec2<float>{ pos.x - scale, pos.y - scale }, gobl::vec2<int>{ scale * 2, scale * 2 },
+	SDLWrapper::DrawSprite(spriteData.name, gobl::vec2<float>{ pos.x, pos.y }, gobl::vec2<int>{ scale, scale },
 		gobl::vec2<int>{ col* spriteData.width, row* spriteData.height }, gobl::vec2<int>{ spriteData.width, spriteData.height });
-
-	/*pge->DrawPartialSprite(pos,
-		olc::vf2d{ (float)32.0f, (float)32.0f },
-		spriteData.sprite,
-		olc::vf2d{ float(col * spriteData.width), float(row * spriteData.height) },
-		olc::vf2d{ float(spriteData.width), float(spriteData.height) }
-	);*/
 }
